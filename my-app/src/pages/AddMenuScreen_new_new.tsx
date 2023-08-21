fe673d40a3064d207bcf4d946254d8e0bfe4de36
Shota Yasuraoka <59210yasuraoka@seiko.ac.jp>
2023-08-21 22:29:13 +0900

    update

--- my-app/src/pages/AddMenuScreen_new_new.tsx
+++ my-app/src/pages/AddMenuScreen_new_new.tsx
@@ -98,7 +98,55 @@ const AddMenuScreenNewNew: React.FC = () => {
     });
 };
 
+const addCustomizeType2 = (settingIndex: number, newCustomization: CustomizeType2) => {
+    setMenu((prevMenu) => {
+      const updatedSettings = prevMenu.settings.map((setting, index) => {
+        if (index !== settingIndex) return setting;
+
+        return {
+          ...setting,
+          customizations: [
+            ...(setting.customizations || []), 
+            newCustomization
+          ]
+        };
+      });
+
+      return { ...prevMenu, settings: updatedSettings };
+    });
+};
+
+const addCustomizeType3 = (settingIndex: number, newCustomization: CustomizeType3) => {
+    setMenu((prevMenu) => {
+      const updatedSettings = prevMenu.settings.map((setting, index) => {
+        if (index !== settingIndex) return setting;
+
+        return {
+          ...setting,
+          customizations: [
+            ...(setting.customizations || []), 
+            newCustomization
+          ]
+        };
+      });
+
+      return { ...prevMenu, settings: updatedSettings };
+    });
+};
+
   function isCustomizeType1(object: any): object is CustomizeType1 {
+    return (
+      object && typeof object.name === "string" && Array.isArray(object.options)
+    );
+  }
+
+  function isCustomizeType2(object: any): object is CustomizeType2 {
+    return (
+      object && typeof object.name === "string" && Array.isArray(object.options)
+    );
+  }
+
+  function isCustomizeType3(object: any): object is CustomizeType3 {
     return (
       object && typeof object.name === "string" && Array.isArray(object.options)
     );
@@ -328,8 +376,319 @@ const AddMenuScreenNewNew: React.FC = () => {
             >
               カスタマイズタイプ1を追加する
             </button>
-          </div>
-        ))}
+
+
+        {/* Customization Type 2 */}
+            {(setting.customizations || []).map((type2, typeIndex) => {
+              if (!isCustomizeType2(type2)) {
+                return null; // or return <></> for an empty fragment if you're using React 16+
+              }
+              // The 'else' branch has the JSX related to CustomizeType1
+              return (
+                <div key={`${settingIndex}-${typeIndex}`}>
+                  名前:
+                  <input
+                    type="text"
+                    placeholder="Name"
+                    value={type2.name}
+                    onChange={(e) => {
+    const newSettings = [...menu.settings];
+    if (
+        newSettings[settingIndex] &&
+        newSettings[settingIndex].customizations
+    ) {
+        (newSettings[settingIndex].customizations as unknown as CustomizeType2[])[typeIndex].name = e.target.value;
+    }
+    setMenu((prev) => ({ ...prev, settings: newSettings }));
+}}
+
+                  />
+                  オプション:
+                  {type2.options.map((option, optionIndex) => (
+                    <div key={optionIndex}>
+                      <input
+                        type="text"
+                        placeholder={`Option ${optionIndex + 1}`}
+                        value={option.optionName}
+                        onChange={(e) => {
+                          const newSettings = [...menu.settings];
+                          if (
+                            newSettings[settingIndex] &&
+                            newSettings[settingIndex].customizations
+                          ) {
+                            (
+                              newSettings[settingIndex]
+                                .customizations as CustomizeType2[]
+                            )[typeIndex].options[optionIndex] = e.target.value;
+                          }
+                          setMenu((prev) => ({
+                            ...prev,
+                            settings: newSettings,
+                          }));
+                        }}
+                      />
+                      <input
+                    type="number"
+                    placeholder="Price"
+                    value={option.price}
+                    onChange={(e) => {
+                      const updatedOptions = [...type.options];
+                      updatedOptions[optionIndex].price = parseFloat(
+                        e.target.value,
+                      );
+                      const newTypes = [...menu.customizeType2];
+                      newTypes[index].options = updatedOptions;
+                      setMenu((prev) => ({
+                        ...prev,
+                        customizeType2: newTypes,
+                      }));
+                    }}
+                  />
+                      <button
+                        onClick={() => {
+                          const newSettings = [...menu.settings];
+                          if (
+                            newSettings[settingIndex] &&
+                            newSettings[settingIndex].customizations
+                          ) {
+                            (
+                              newSettings[settingIndex]
+                                .customizations as CustomizeType1[]
+                            )[typeIndex].options.splice(optionIndex, 1);
+                          }
+
+                          setMenu((prev) => ({
+                            ...prev,
+                            settings: newSettings,
+                          }));
+                        }}
+                      >
+                        削除
+                      </button>
+                    </div>
+                  ))}
+                  <button
+                    onClick={() => {
+                      const newSettings = [...menu.settings];
+                      if (
+                        newSettings[settingIndex] &&
+                        newSettings[settingIndex].customizations
+                      ) {
+                        (
+                          newSettings[settingIndex]
+                            .customizations as CustomizeType1[]
+                        )[typeIndex].options.push("");
+                        setMenu((prev) => ({ ...prev, settings: newSettings }));
+                      }
+                    }}
+                  >
+                    追加
+                  </button>
+                  <div>
+                    <span>デフォルト: {type.default}</span>
+                    <select
+                      value={type.default}
+                      onChange={(e) => {
+                        const newSettings = [...menu.settings];
+                        if (
+                          newSettings[settingIndex] &&
+                          newSettings[settingIndex].customizations
+                        ) {
+                          (
+                            newSettings[settingIndex]
+                              .customizations as CustomizeType1[]
+                          )[typeIndex].default = e.target.value;
+                          setMenu((prev) => ({
+                            ...prev,
+                            settings: newSettings,
+                          }));
+                        }
+                      }}
+                    >
+                      {type.options.map((option, optionIndex) => (
+                        <option key={optionIndex} value={option}>
+                          {option}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                  <button
+                    onClick={() => {
+                      const newSettings = [...menu.settings];
+                      if (
+                        newSettings[settingIndex] &&
+                        newSettings[settingIndex].customizations
+                      ) {
+                        (
+                          newSettings[settingIndex]
+                            .customizations as CustomizeType1[]
+                        ).splice(typeIndex, 1);
+                        setMenu((prev) => ({ ...prev, settings: newSettings }));
+                      }
+                    }}
+                  >
+                    このカスタマイズを削除する
+                  </button>
+                </div>
+              );
+            })}
+            <button
+              onClick={() =>
+                addCustomizeType1(settingIndex, defaultCustomizeType1)
+              }
+            >
+              カスタマイズタイプ1を追加する
+            </button>
+          
+          {/* Customization Type 1 */}
+            {(setting.customizations || []).map((type, typeIndex) => {
+              if (!isCustomizeType1(type)) {
+                return null; // or return <></> for an empty fragment if you're using React 16+
+              }
+              // The 'else' branch has the JSX related to CustomizeType1
+              return (
+                <div key={`${settingIndex}-${typeIndex}`}>
+                  名前:
+                  <input
+                    type="text"
+                    placeholder="Name"
+                    value={type.name}
+                    onChange={(e) => {
+                      const newSettings = [...menu.settings];
+                      if (
+                        newSettings[settingIndex] &&
+                        newSettings[settingIndex].customizations
+                      ) {
+                        (
+                          newSettings[settingIndex]
+                            .customizations as CustomizeType1[]
+                        )[typeIndex].name = e.target.value;
+                      }
+                      setMenu((prev) => ({ ...prev, settings: newSettings }));
+                    }}
+                  />
+                  オプション:
+                  {type.options.map((option, optionIndex) => (
+                    <div key={optionIndex}>
+                      <input
+                        type="text"
+                        placeholder={`Option ${optionIndex + 1}`}
+                        value={option}
+                        onChange={(e) => {
+                          const newSettings = [...menu.settings];
+                          if (
+                            newSettings[settingIndex] &&
+                            newSettings[settingIndex].customizations
+                          ) {
+                            (
+                              newSettings[settingIndex]
+                                .customizations as CustomizeType1[]
+                            )[typeIndex].options[optionIndex] = e.target.value;
+                          }
+                          setMenu((prev) => ({
+                            ...prev,
+                            settings: newSettings,
+                          }));
+                        }}
+                      />
+
+                      <button
+                        onClick={() => {
+                          const newSettings = [...menu.settings];
+                          if (
+                            newSettings[settingIndex] &&
+                            newSettings[settingIndex].customizations
+                          ) {
+                            (
+                              newSettings[settingIndex]
+                                .customizations as CustomizeType1[]
+                            )[typeIndex].options.splice(optionIndex, 1);
+                          }
+
+                          setMenu((prev) => ({
+                            ...prev,
+                            settings: newSettings,
+                          }));
+                        }}
+                      >
+                        削除
+                      </button>
+                    </div>
+                  ))}
+                  <button
+                    onClick={() => {
+                      const newSettings = [...menu.settings];
+                      if (
+                        newSettings[settingIndex] &&
+                        newSettings[settingIndex].customizations
+                      ) {
+                        (
+                          newSettings[settingIndex]
+                            .customizations as CustomizeType1[]
+                        )[typeIndex].options.push("");
+                        setMenu((prev) => ({ ...prev, settings: newSettings }));
+                      }
+                    }}
+                  >
+                    追加
+                  </button>
+                  <div>
+                    <span>デフォルト: {type.default}</span>
+                    <select
+                      value={type.default}
+                      onChange={(e) => {
+                        const newSettings = [...menu.settings];
+                        if (
+                          newSettings[settingIndex] &&
+                          newSettings[settingIndex].customizations
+                        ) {
+                          (
+                            newSettings[settingIndex]
+                              .customizations as CustomizeType1[]
+                          )[typeIndex].default = e.target.value;
+                          setMenu((prev) => ({
+                            ...prev,
+                            settings: newSettings,
+                          }));
+                        }
+                      }}
+                    >
+                      {type.options.map((option, optionIndex) => (
+                        <option key={optionIndex} value={option}>
+                          {option}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                  <button
+                    onClick={() => {
+                      const newSettings = [...menu.settings];
+                      if (
+                        newSettings[settingIndex] &&
+                        newSettings[settingIndex].customizations
+                      ) {
+                        (
+                          newSettings[settingIndex]
+                            .customizations as CustomizeType1[]
+                        ).splice(typeIndex, 1);
+                        setMenu((prev) => ({ ...prev, settings: newSettings }));
+                      }
+                    }}
+                  >
+                    このカスタマイズを削除する
+                  </button>
+                </div>
+              );
+            })}
+            <button
+              onClick={() =>
+                addCustomizeType1(settingIndex, defaultCustomizeType1)
+              }
+            >
+              カスタマイズタイプ1を追加する
+            </button>
+
+
         <button onClick={() => addSetting(defaultSetting)}>
           メニュー詳細カテゴリーを追加する
         </button>
