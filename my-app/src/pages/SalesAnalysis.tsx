4dc0144abb0a370bb7f0bbee49cad9cc1e57388f
YukiYamamoto-CityTech <yk-yamamoto41@g.ecc.u-tokyo.ac.jp>
2023-10-02 13:00:41 +0900

    売上分析デザイン（通常分析）

--- my-app/src/pages/SalesAnalysis.tsx
+++ my-app/src/pages/SalesAnalysis.tsx
@@ -12,7 +12,7 @@ import "react-datepicker/dist/react-datepicker.css";
 
 Chart.register(...registerables)
 const SalesAnalysis : React.FC = () => {
-    const SalesSpanOption = ["時間帯別", "日別", "週間別", "曜日別", "商品別"]
+    const SalesSpanOption = ["時間帯別", "日別", "月別", "曜日別"]
     const SalesTypeOption = ["総売上", "純売上", "粗利益", "営業利益"]
     const SalesByMenu : {[key: string]: string|number;}[] = [{name:"醤油ラーメン", count:9, price:8100},{name:"醤油ラーメン", count:9, price:8100},{name:"醤油ラーメン", count:9, price:8100}]
     const SalesPerHour : {[key: number]: {[key: string]: number|string};} = {
@@ -33,28 +33,75 @@ const SalesAnalysis : React.FC = () => {
         23:{sales: 2000, priceClass: "normal"},
     }
     const SelectedHourOption = [8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
-    const SalesDataSet = {
-        labels : Object.keys(SalesPerHour),
-        datasets: [{
-            label: '時間ごとの売上',
-            data : Object.values(SalesPerHour).map((value, index) => (value.sales)),
-            backgroundColor : Object.values(SalesPerHour).map((value, index) => (value.priceClass==="normal" ? "rgba(255, 204, 0, 1)" : "rgba(255, 127, 109, 1)"))
-        }]
-    }
-
-  console.log(SalesDataSet);
-
-
+    const salesDetailData = {"純売上": 19800, "純売上(税別)": 18000, "消費税": 1800, "総売上": 18000, "値引き": 0, "原価": 4500, "粗利益": 13500, "販売費": 11500, "営業利益": 2000, "客数": 22, "価格変動": 2.5}
+    const startDate = [2021,10,1]
     const Today = new Date();
     const DayName = ["日","月","火","水","木","金","土"]
+    const YearMonthList: {[key: string]: number[]} = Object()
+    for(let i = startDate; i[0] < Number(format(Today, 'yyyy')) || (i[0] === Number(format(Today, 'yyyy')) && i[1] <= Number(format(Today, 'MM')));){
+        if(Object.keys(YearMonthList).includes(String(i[0]))){
+            YearMonthList[String(i[0])].push(i[1])
+        }else{
+            YearMonthList[String(i[0])] = [i[1]]
+        }
+        if(i[1]!==12){
+            i[1]++
+        }else{
+            i[0]++
+            i[1]=1
+        }
+    }
     const [selectedSalesSpan, setSelectedSalesSpan] = useState("時間帯別")
     const [selectedSalesType, setSelectedSalesType] = useState("総売上")
     const [selectedAnalysisType, setSelectedAnalysisType] = useState("通常売上")
     const [selectedHour, setSelectedHour] = useState(14)
-    const [selectedDate, setSelectedDate] = useState<Date | null>(Today);
+    const [selectedDate, setSelectedDate] = useState<Date>(Today);
+    const [selectedYear, setSelectedYear] = useState<Number>(Number(format(Today, 'yyyy')));
+    const [selectedMonth, setSelectedMonth] = useState<Number>(Number(format(Today, 'MM')));
+    const [selectedDay, setSelectedDay] = useState<String>(DayName[Number(format(Today, 'e'))]);
+    const dateList : Date[] = [];
+    for(let i = 1; i < 8; i++){
+        const settingDate: Date = new Date(Today);
+        settingDate.setDate(Today.getDate() - Number(format(Today, 'e')) + i)
+        dateList.push(settingDate)
+    }
+    const [selectedWeek, setSelectedWeek] = useState<Date[]>(dateList);
 
-  const [displayTable, setDisplayTable] = useState(false);
 
+    const [displayTable, setDisplayTable] = useState(false);
+
+    const SalesDataSet = {
+        labels : Object.keys(SalesPerHour),
+        datasets: [{
+            data : Object.values(SalesPerHour).map((value, index) => (value.sales)),
+            backgroundColor : Object.keys(SalesPerHour).map((key, index) => (Number(key)===selectedHour ? "rgba(255, 240, 180, 1)" : SalesPerHour[Number(key)].priceClass==="normal" ? "rgba(217, 217, 217, 1)" : "rgba(153, 153, 153, 1)")),
+            borderRadius : 5, 
+        }]
+    }
+    const options: {} = {
+        plugins: {
+            legend: {
+              display: false, // データセットのラベルを非表示にする
+            },
+        },
+        scales: {
+            x: {
+                position: 'bottom',
+                grid: {
+                    display: false, // X軸のグリッドラインを非表示にする
+                },
+            },
+            y: {
+                position: 'right',
+                ticks: {
+                    stepSize: 10000, // メモリの間隔を指定します
+                },
+            }
+        },
+        responsive: true,
+    }
+
+    console.log(YearMonthList);
 
     const selectedSalesSpanChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
         setSelectedSalesSpan(event.target.value);
@@ -65,9 +112,26 @@ const SalesAnalysis : React.FC = () => {
     const selectedHourChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
         setSelectedHour(Number(event.target.value));
     };
-    const handleDateChange = (date: Date | null) => {
+    const handleDateChange = (date: Date) => {
         setSelectedDate(date);
+        if (date) {
+            const dateList : Date[] = [];
+            for(let i = 1; i < 8; i++){
+                const settingDate: Date = new Date(date);
+                settingDate.setDate(date.getDate() - Number(format(date, 'e')) + i)
+                dateList.push(settingDate)
+            }
+            setSelectedWeek(dateList)
+        }
     };
+    const highlightCustomDates = (date: Date | null) => {
+        if(date){
+            if (selectedWeek.includes(date)) {
+                return 'custom-highlight'; // カスタムスタイルを適用するクラス名を返す
+            }
+        }
+        return null;
+      };
     const customDateFormat = (date: Date | null) => {
         if (date) {
             return `${format(date, 'M月dd日')} (${DayName[Number(format(date, 'e'))]})`;
@@ -100,6 +164,8 @@ const SalesAnalysis : React.FC = () => {
         </div>
         <div className="display-option">
             <div className="display-span-option">
+                {selectedSalesSpan === "時間帯別" ?
+                <>
                 <DatePicker
                     selected={selectedDate}
                     onChange={handleDateChange}
@@ -112,6 +178,41 @@ const SalesAnalysis : React.FC = () => {
                         <option value={value}>{value}時〜{value+1}時</option>
                     ))}
                 </select>
+                </> : 
+                selectedSalesSpan === "日別" ? 
+                <>
+                <div className="display-week-select">{format(selectedDate, 'M月')}第{Math.floor((selectedDate.getDate() - Number(format(selectedDate, 'e')) + 5)/7)+1}週</div>
+                <DatePicker
+                    selected={selectedDate}
+                    onChange={handleDateChange}
+                    dateFormat={customDateFormat(selectedDate)} // 日付の表示形式を指定
+                    placeholderText="日付を選択" // プレースホルダーテキスト
+                    className="display-date"
+                    highlightDates={selectedWeek}
+                    dayClassName={highlightCustomDates}
+                />
+                </> : 
+                selectedSalesSpan === "月別" ? 
+                <>
+                <select className="display-year-select" value={String(selectedYear)} onChange={(e) => setSelectedYear(Number(e.target.value))}>
+                    {Object.keys(YearMonthList).map((key, index) => (
+                        <option value={key}>{key}年</option>
+                    ))}
+                </select>
+                <select className="display-month-select" value={String(selectedMonth)} onChange={(e) => setSelectedMonth(Number(e.target.value))}>
+                    {YearMonthList[String(selectedYear)].map((value, index) => (
+                        <option value={value}>{value}月</option>
+                    ))}
+                </select>
+                </> : 
+                <>
+                <select className="display-day-select" value={String(selectedDay)} onChange={(e) => setSelectedDay(e.target.value)}>
+                    {DayName.map((value, index) => (
+                        <option value={value}>{value}曜日</option>
+                    ))}
+                </select>
+                </>
+                }
                 <div style={{fontSize: "16px", width: "30px", textAlign: "center"}}>の</div>
                 <select className="sales-type-select" value={selectedSalesType} onChange={selectedSalesTypeChange}>
                     {SalesTypeOption.map((value, index) => (
@@ -134,7 +235,7 @@ const SalesAnalysis : React.FC = () => {
             <div className="sales-datas">
                 <div className="sales-total">
                     <p className="sales-type-text">{selectedSalesType}</p>
-                    <p className="sales-type-price">¥ 18000</p>
+                    <p className="sales-type-price">¥ 18,000</p>
                 </div>
                 <div className="sales-menu">
                     <div className="sales-menu-headline">
@@ -145,45 +246,69 @@ const SalesAnalysis : React.FC = () => {
                     {SalesByMenu.map((data,index) => (
                         <div className="menu-text">
                             <div className="menu-name">{data.name}</div>
-                            <div className="menu-count">{data.count}</div>
-                            <div className="menu-price">{data.price}円</div>
+                            <div className="menu-count">{data.count.toLocaleString()}</div>
+                            <div className="menu-price">{data.price.toLocaleString()}円</div>
                         </div>
                     ))}
                 </div>
             </div>
             <div className="graph-and-calendar">
-                {!displayTable ? <div className="sales-per-hour-graph-area">
-                    <Bar data={SalesDataSet}/>
+                {!displayTable ? 
+                <div className="sales-per-hour-graph-area">
+                    <Bar data={SalesDataSet} options={options} className="bar-graph"/>
                 </div> :
                 <div className="sales-per-hour-table-area">
                     <div className="table-col">
                         <div className="table-hour-col">
                             {Object.keys(SalesPerHour).map((key, index) => (
-                                index < Object.keys(SalesPerHour).length/2 && <div className="table-text">{key}</div>
+                                index < Object.keys(SalesPerHour).length/2 && <div className="table-text" style={{height: String(100/(Object.keys(SalesPerHour).length/2))+"%", backgroundColor: Number(key)===selectedHour ? "#FFF0B4" : "rgba(0,0,0,0)"}}>{key}時</div>
                             ))}
                         </div>
                         <div className="table-sales-col">
-                            {Object.values(SalesPerHour).map((value, index) => (
-                                index < Object.keys(SalesPerHour).length/2 && <div className="table-text">¥ {value.sales}</div>
+                            {Object.keys(SalesPerHour).map((key, index) => (
+                                index < Object.keys(SalesPerHour).length/2 && <div className="table-text" style={{height: String(100/(Object.keys(SalesPerHour).length/2))+"%", backgroundColor: Number(key)===selectedHour ? "#FFF0B4" : "rgba(0,0,0,0)"}}>{SalesPerHour[Number(key)].sales.toLocaleString()}円</div>
                             ))}
                         </div>
                     </div>
                     <div className="table-col">
                         <div className="table-hour-col">
                             {Object.keys(SalesPerHour).map((key, index) => (
-                                index >= Object.keys(SalesPerHour).length/2 && <div className="table-text">{key}</div>
+                                index >= Object.keys(SalesPerHour).length/2 && <div className="table-text" style={{height: String(100/(Object.keys(SalesPerHour).length/2))+"%", backgroundColor: Number(key)===selectedHour ? "#FFF0B4" : "rgba(0,0,0,0)"}}>{key}時</div>
                             ))}
-                            {Object.keys(SalesPerHour).length%2 === 1 && <div className="table-text">　</div>}
+                            {Object.keys(SalesPerHour).length%2 === 1 && <div className="table-text" style={{height: String(100/(Object.keys(SalesPerHour).length/2))+"%"}}>　</div>}
                         </div>
                         <div className="table-sales-col">
-                            {Object.values(SalesPerHour).map((value, index) => (
-                                index >= Object.keys(SalesPerHour).length/2 && <div className="table-text">¥ {value.sales}</div>
+                            {Object.keys(SalesPerHour).map((key, index) => (
+                                index >= Object.keys(SalesPerHour).length/2 && <div className="table-text" style={{height: String(100/(Object.keys(SalesPerHour).length/2))+"%", backgroundColor: Number(key)===selectedHour ? "#FFF0B4" : "rgba(0,0,0,0)"}}>{SalesPerHour[Number(key)].sales.toLocaleString()}円</div>
                             ))}
-                            {Object.keys(SalesPerHour).length%2 === 1 && <div className="table-text">　</div>}
+                            {Object.keys(SalesPerHour).length%2 === 1 && <div className="table-text" style={{height: String(100/(Object.keys(SalesPerHour).length/2))+"%"}}>　</div>}
                         </div>
                     </div>
                 </div>}
-
+                <div className="sales-detail-data">
+                    <div className="sales-detail-col">
+                        <div className="row-1"><div className="row-name">純売上</div><div className="row-item">{salesDetailData["純売上"].toLocaleString()}円</div></div>
+                        <div className="row-2"><div className="row-name">純売上(税別)</div><div className="row-item">{salesDetailData["純売上(税別)"].toLocaleString()}円</div></div>
+                        <div className="row-1"><div className="row-name">消費税</div><div className="row-item">{salesDetailData["消費税"].toLocaleString()}円</div></div>
+                        <div className="row-2"><div className="row-name">総売上</div><div className="row-item">{salesDetailData["総売上"].toLocaleString()}円</div></div>
+                        <div className="row-1"><div className="row-name">値引き</div><div className="row-item">{salesDetailData["値引き"].toLocaleString()}円</div></div>
+                        <div className="row-2"><div className="row-name">原価</div><div className="row-item">{salesDetailData["原価"].toLocaleString()}円</div></div>
+                        <div className="row-1"><div className="row-name">粗利益</div><div className="row-item">{salesDetailData["粗利益"].toLocaleString()}円</div></div>
+                        <div className="row-2"><div className="row-name">販売費</div><div className="row-item">{salesDetailData["販売費"].toLocaleString()}円</div></div>
+                        <div className="row-1"><div className="row-name">営業利益</div><div className="row-item">{salesDetailData["営業利益"].toLocaleString()}円</div></div>
+                    </div>
+                    <div className="sales-detail-col">
+                        <div className="row-3"><div className="row-name">客数</div><div className="row-item">{salesDetailData["客数"].toLocaleString()}人</div></div>
+                        <div className="row-3"><div className="row-name">価格変動</div><div className="row-item">{salesDetailData["価格変動"].toLocaleString()}％</div></div>
+                        <div className="row-3"><div className="row-name"></div><div className="row-item"></div></div>
+                        <div className="row-3"><div className="row-name"></div><div className="row-item"></div></div>
+                        <div className="row-3"><div className="row-name"></div><div className="row-item"></div></div>
+                        <div className="row-3"><div className="row-name"></div><div className="row-item"></div></div>
+                        <div className="row-3"><div className="row-name"></div><div className="row-item"></div></div>
+                        <div className="row-3"><div className="row-name"></div><div className="row-item"></div></div>
+                        <div className="row-3"><div className="row-name"></div><div className="row-item"></div></div>
+                    </div>
+                </div>
             </div>
         </div>
       </div>
