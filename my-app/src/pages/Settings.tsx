45fc8c258812b4a4ad70b3fbd7d700e7d44443d8
HImajo <hiroto-imajo1610@g.ecc.u-tokyo.ac.jp>
2023-10-29 23:13:19 +0900

    settingbarの表示 Settings.cssと.tsxの追加

---
+++ my-app/src/pages/Settings.tsx
@@ -0,0 +1,12 @@
+import "./Settings.css";
+import SettingBar from "../header/SettingBar";
+
+function Settings() {
+    return (
+        <div>
+          <SettingBar focusButton="settings" />
+        </div>
+    );
+}
+
+export default Settings;
