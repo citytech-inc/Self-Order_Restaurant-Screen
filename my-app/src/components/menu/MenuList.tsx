ea96190dee327ba947c049d2d44a6e8e47bdb7cf
HImajo <hiroto-imajo1610@g.ecc.u-tokyo.ac.jp>
2023-12-19 09:38:43 +0900

    カスタマイズありの見た目の修正、商品のクリックの修正

--- my-app/src/components/menu/MenuList.tsx
+++ my-app/src/components/menu/MenuList.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, {useState} from "react";
 import "./MenuList.css";
 
 const MenuList: React.FC = () => {
+  const [selectedItem, setSelectedItem] = useState<number | null>(null);
   // List of items to display - replace these with your actual data
   const items = [
     "醤油ラーメン",
@@ -11,16 +12,28 @@ const MenuList: React.FC = () => {
     "烏龍茶",
   ];
 
+  const handleItemClick = (index: number) => {
+    setSelectedItem(index);
+  };
+
+  const resetSelection = () => {
+    setSelectedItem(null);
+  };
+
   return (
     <div className="list-screen">
       <div className="list-container">
         {items.map((item, index) => (
-          <div key={index} className="list-item">
-            {item} {/* Replace with your actual content */}
+          <div
+            key={index}
+            className={`list-item ${selectedItem === index ? "selected" : ""}`}
+            onClick={() => handleItemClick(index)}
+          >
+            {item}
           </div>
         ))}
       </div>
-      <button className="fixed-button">+ 商品を追加</button>
+      <button className="fixed-button" onClick={resetSelection}>+ 商品を追加</button>
     </div>
   );
 };
